684776c5dcaebedd1ba5de34be0b55bd4104a237
Jordach <jordach@wolfo.tech>
2024-09-23 00:37:40 +0100

    Correct multiplier for test ratio

--- test_bucketer.py
+++ test_bucketer.py
@@ -30,6 +30,6 @@ bucketer = Bucketeer(
 
 for ratio in test_ratios:
 	if ratio < 1:
-		bucketer.test_resize(1000*ratio, 1000, emit_print=True)
+		bucketer.test_resize(1000/ratio, 1000, emit_print=True)
 	else:
 		bucketer.test_resize(1000, 1000*ratio, emit_print=True)
