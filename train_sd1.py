f157468cee1889a5ba8b1c28cb0699fbca26c335
Jordach <jordach@wolfo.tech>
2024-09-23 05:08:45 +0100

    Rename yaml_config to just yaml

--- train_sd1.py
+++ train_sd1.py
@@ -42,7 +42,7 @@ def main():
     args = parse_args()
     
     # Load settings from YAML config
-    with open(args.yaml_config, "r") as f:
+    with open(args.yaml, "r") as f:
         settings = yaml.safe_load(f)
 
     accelerator = Accelerator(
